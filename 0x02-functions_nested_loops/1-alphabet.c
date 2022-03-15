a1b6ed1f93ceecebce6bb16ab2280f2fa344a336
soroel <sorophinenoel@gmail.com>
2022-03-15 14:14:21 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,9 @@
 #include "main.h"
-<<<<<<< HEAD
-/** 
- * print_alphabet -prints the english alphabet from a-z.
-=======
+#include stdio.h
+/**
+* print_alphabet -prints the english alphabet from a-z.
 /**
  * print_alphabet - prints the english alphabet from a-z.
->>>>>>> 5a5b2b0decfd8551c4cd3e86b50d0dfcbabdaca3
  * Return: Always 0.
  */
 void print_alphabet(void)
