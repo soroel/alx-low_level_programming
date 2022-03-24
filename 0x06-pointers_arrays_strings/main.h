e24e7bb7975b1fa23f14f69b1572b8a123322761
soroel <sorophinenoel@gmail.com>
2022-03-24 06:45:06 +0000

    strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef FILE_MAIN
-#define FILE_MAIN
+#ifndef MAIN_FILE
+#define MAIN_FILE
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
