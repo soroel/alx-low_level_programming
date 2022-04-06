160ba844eb42ca4cfffefa38df39766575de7efb
soroel <85451202+soroel@users.noreply.github.com>
2022-04-06 05:12:48 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,77 +15,35 @@
  * Return: pointer..
 
  */
-
 int **alloc_grid(int width, int height)
   
 {
-  
-  int **s, r, c;
-  
-
-  
-  if (width <= 0 || height <= 0)
-    
-    {
-      
-      return (NULL);
-      
-    }
-  
-
-  
-  s = malloc(sizeof(int *) * height);
-  
-  if (s == NULL)
-    
-    {
-      
-      return (NULL);
-      
-    }
-  
-
-  
-  for (r = 0; r < height; r++)
-    
-    {
-      
-      s[r] = malloc(sizeof(int) * width);
-      
-
-      
-      if (s[r] == NULL)
-	
+	int **s, r, c;
+	if (width <= 0 || height <= 0)
 	{
-	  
-	  for (; r >= 0; r--)
-	    
-	    {
-	      
-	      free(s[r]);
-	      
-	    }
-	  
-	  free(s);
-	  
-	  return (NULL);
-	  
+		return (NULL);
 	}
-      
-
-      
-      for (c = 0; c <= width; c++)
-	
+	s = malloc(sizeof(int *) * height);
+	if (s == NULL)
 	{
-	  
-	  s[r][c] = 0;
-	  
+		return (NULL);
 	}
-      
-    }
-  
-  return (s);
-  
-
-  
+	for (r = 0; r < height; r++)
+	{
+		s[r] = malloc(sizeof(int) * width);
+		if (s[r] == NULL)
+		{
+			for (; r >= 0; r--)
+			{
+				free(s[r]);
+			}
+			free(s);
+			return (NULL);
+		}
+		for (c = 0; c <= width; c++)
+		{
+			s[r][c] = 0;
+		}
+	}
+	return (s);
 }
