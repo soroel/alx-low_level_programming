fb929c4140c422551590f9e535713c30d9485af0
soroel <85451202+soroel@users.noreply.github.com>
2022-04-06 06:25:47 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -18,36 +16,97 @@ return (size);
 }
 
 /**
- * *argstostr - description
- * @ac: int
- * @av: arguments
- * Return: string
+ * *str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer
  */
 
-char *argstostr(int ac, char **av)
+char *str_addChar (char *str, char c)
 {
-int i = 0, nc = 0, j = 0, cmpt = 0;
-char *s;
+int size, i;
+char *m;
 
-if (ac == 0 || av == NULL)
-	return (NULL);
+size = _strlen(str);
 
-for (; i < ac; i++, nc++)
-	nc += _strlen(av[i]);
+m = malloc((size + 1) * sizeof(char) + 1);
+if (m == 0)
+	return (0);
 
-s = malloc(sizeof(char) * nc + 1);
-if (s == 0)
-	return (NULL);
+for (i = 0; i <= size; i++)
+	m[i] = str[i];
 
-for (i = 0; i < ac; i++)
-{
-	for (j = 0; av[i][j] != '\0'; j++, cmpt++)
-		s[cmpt] = av[i][j];
+m[i + 1] = c;
+m[i + 2] = '\0';
 
-	s[cmpt] = '\n';
-	cmpt++;
+return (m);
 }
-s[cmpt] = '\0';
 
-return (s);
+
+/**
+ * *nbr_spaces - return the number of occurent of a string
+ * @s: string to check
+ * Return: int
+ */
+
+unsigned int nbr_spaces(char *s)
+{
+	int i, cmpt = 0;
+
+	for (i = 0; s[i + 1] != '\0'; i++)
+	{
+		if (s[i]  == ' ' && s[i + 1] != ' ')
+			cmpt++;
+	}
+
+	return (cmpt + 1);
+}
+
+
+/**
+  *strtow - split a sentence into multiple words.
+  *@str: the string passed as argument.
+  *Return: tokens
+  */
+char **strtow(char *str)
+{
+int i;
+int spaces = nbr_spaces(str);
+char **tokens = NULL;//malloc(sizeof(char *) * (spaces));
+char *token;
+int checkingSpace = 0;
+int word = 0;
+
+if (!tokens)
+{
+	printf("Failed");
+	return (0);
+}
+	
+
+printf("looping");
+for (i = 0; str[i] != '\0'; i++)
+{
+	if (str[i] == ' ')
+	{
+		if (checkingSpace == 0)
+		{
+			word++;
+			checkingSpace = 1;
+		} 
+	}
+	else
+	{
+		printf("1");
+		token = tokens[word];
+		free(tokens[word]);
+		str_addChar(token, str[i]);
+		checkingSpace = 0;
+	}
+
+}
+
+tokens[i] = NULL;
+
+return (tokens);
 }
