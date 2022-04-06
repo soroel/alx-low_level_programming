95323ab0292bc87bbfd5b97b50b66c052c102e02
soroel <85451202+soroel@users.noreply.github.com>
2022-04-06 05:18:16 +0300

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 
 #include <stdlib.h>
-
 /**
-
  * alloc_grid - prints a grid of integers
-
  * @width: width of the grid
-
  * @height: height of the grid
-
- *
-
  * Return: pointer..
-
  */
 int **alloc_grid(int width, int height)
   
