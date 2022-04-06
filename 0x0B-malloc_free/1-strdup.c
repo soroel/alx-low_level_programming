9857ac464d12076a2aa21fa71642188a00bb577b
soroel <85451202+soroel@users.noreply.github.com>
2022-04-06 06:10:15 +0300

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,63 +1,32 @@
 #include <stdlib.h>
 
-
-
 /**
-
  * _strdup - copy
-
  * @str: pointer to string
-
  * Return: pointer
-
  */
 
-
-
 char *_strdup(char *str)
-  
 {
-  
-  char *s;
-  
-  int i = 0, j;
-  
+	char *s;
+	int i = 0, j;
 
-  
-  if (!str)
-    
-    return (NULL);
-  
+	if (!str)
+		return (NULL);
 
-  
-  while (*(str + i))
-    
-    i++;
-  
-  i++;
-  
-  s = malloc(sizeof(char) * i);
-  
+	while (*(str + i))
+		i++;
+	i++;
+	s = malloc(sizeof(char) * i);
 
-  
-  if (s == NULL)
-    
-    {
-      
-      return (NULL);
-      
-    }
-  
+	if (s == NULL)
+	{
+		return (NULL);
+	}
 
-  
-  for (j = 0; j <= i; j++)
-    
-    {
-      
-      s[j] = str[j];
-      
-    }
-  
-  return (s);
-  
+	for (j = 0; j <= i; j++)
+	{
+		s[j] = str[j];
+	}
+	return (s);
 }
